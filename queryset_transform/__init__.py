c6942fd8ad26d4d31e07fa6ef1554b6d83501255
Chris Adams <chris@improbable.org>
2015-03-10 16:14:49 -0400

    Support for ValuesQuerySet / ValuesListQuerySet

--- queryset_transform/__init__.py
+++ queryset_transform/__init__.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+
 class TransformQuerySetMixin(object):
     def __init__(self, *args, **kwargs):
         super(TransformQuerySetMixin, self).__init__(*args, **kwargs)
         self._transform_fns = []
 
-    def _clone(self, klass=None, setup=False, **kw):
-        c = super(TransformQuerySetMixin, self)._clone(klass, setup, **kw)
+    def _clone(self, klass=None, setup=False, **kwargs):
+        c = super(TransformQuerySetMixin, self)._clone(klass, setup, **kwargs)
+        c.__dict__.update(kwargs)
         c._transform_fns = self._transform_fns[:]
         return c
 
@@ -24,8 +26,38 @@ class TransformQuerySetMixin(object):
             return iter(results)
         return result_iter
 
+    def values(self, *fields):
+        new_qs = self._clone(klass=TransformValuesQuerySet, setup=True, _fields=fields)
+        # We have to clear any existing transforms as they will expect a different result type
+        # but want to allow adding new ones:
+        del new_qs._transform_fns[:]
+        return new_qs
+
+    def values_list(self, *fields, **kwargs):
+        flat = kwargs.pop('flat', False)
+
+        if kwargs:
+            raise TypeError('Unexpected keyword arguments to values_list: %s' % (list(kwargs),))
+
+        if flat and len(fields) > 1:
+            raise TypeError("'flat' is not valid when values_list is called with more than one field.")
+
+        new_qs = self._clone(klass=TransformValuesListQuerySet, setup=True, _fields=fields, flat=flat)
+        # We have to clear any existing transforms as they will expect a different result type
+        # but want to allow adding new ones:
+        del new_qs._transform_fns[:]
+        return new_qs
+
 
 class TransformQuerySet(TransformQuerySetMixin, models.query.QuerySet):
+    pass
+
+
+class TransformValuesQuerySet(TransformQuerySetMixin, models.query.ValuesQuerySet):
+    pass
+
+
+class TransformValuesListQuerySet(TransformQuerySetMixin, models.query.ValuesListQuerySet):
     pass
 
 
